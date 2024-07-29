bc006b2a6c566c702a7bb107fd6505d181cf0b8b
Paul Vudmaska <paul@vudmaska.com>
2024-07-29 12:19:38 -0500

    init from template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_26_093700) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_29_170144) do
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "content"
